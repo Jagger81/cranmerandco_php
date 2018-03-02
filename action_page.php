72fa76bda7ecdbc5e8c41317049899860a0391c8
Jagger81 <eddie.cranmer@concern.net>
2018-03-02 22:19:32 +0000

    Contact Form Testing

---
+++ action_page.php
@@ -0,0 +1,12 @@
+<?
+
+$firstname = $_POST('firstname');
+$lastname = $_POST('lastname');
+$email = $_POST('email');
+$category = $_POST('category');
+$subject = $_POST('subject');
+
+echo $firstname;
+echo $lastname;
+
+?>
